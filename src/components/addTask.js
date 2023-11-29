715709c12c7bc95b6fc51bca862d463ff6148cae
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-29 17:34:30 +0530

    style task list page

--- src/components/addTask.js
+++ src/components/addTask.js
@@ -6,7 +6,7 @@ export const AddTask = () => {
   const taskList = JSON.parse(localStorage.getItem('todos')) || [];
   const [taskName, setTaskName] = useState('')
   const [taskDescription, setTaskDescription] = useState('')
-  const [taskPriority, setTaskPriority] = useState('low')
+  const [taskPriority, setTaskPriority] = useState('Medium')
 
   const navigate = useNavigate()
 
@@ -27,15 +27,15 @@ export const AddTask = () => {
     <div className='text-center mt-5'>
       <h1>Add Task</h1>
       <div>
-        <label for='task-name'>Task Name <span>*</span></label>
+        <label for='task-name'>Task Name <span className='text-danger fw-bold'>*</span></label>
         <input className='form-control' type='text' id='task-name' onChange={obj => setTaskName(obj.target.value)} />
         <label for='task-description'>Task Description</label>
         <textarea className='form-control' id='task-description' onChange={obj => setTaskDescription(obj.target.value)}></textarea>
         <label for='task-priority'>Priority Level</label>
-        <select className='form-select' id='task-priority' onChange={obj => setTaskPriority(obj.target.value)} defaultValue='medium'>
-          <option value='low' >Low</option>
-          <option value='medium'>Medium</option>
-          <option value='high'>High</option>
+        <select className='form-select' id='task-priority' onChange={obj => setTaskPriority(obj.target.value)}>
+          <option value='Low'>Low</option>
+          <option value='Medium' selected>Medium</option>
+          <option value='High'>High</option>
         </select>
         <button onClick={goToTaskList}> Submit</button>
       </div>
--- src/components/taskList.js
+++ src/components/taskList.js
@@ -15,6 +15,7 @@ export const TaskList = () => {
     updatedTasks[index].isChecked = !updatedTasks[index].isChecked
     localStorage.setItem('todos', JSON.stringify(updatedTasks));
     setTasklist(updatedTasks);
+    // setIsChecked(!isChecked)
   }
 
   const goToAddTask = () => {
@@ -35,23 +36,43 @@ export const TaskList = () => {
   return (
     <div className='text-center mt-5'>
       <h1>Task List</h1>
-      <button onClick={goToAddTask} className='my-5'>Add Task</button>
-      <div className='border border-info'>
-        {
-          taskList.map((task, index) => {
-            return (
-              <div key={index}>
-                <input className='form-check-input' type='checkbox' checked={task.isChecked || false} onChange={()=>handleCheckboxChange(index)} />
-                <div style={{ textDecoration: task.isChecked ? 'line-through' : 'none' }}>{task.name}</div>
-                <div>{task.isChecked ? "Complete" : "Incomplete"}</div>
-                <div>{task.priority}</div>
-                <button onClick={deleteTask.bind(this, index)} disabled={task.isChecked}>Delete</button>
-                <button onClick={goToEditTask.bind(this, index)} disabled={task.isChecked}>Edit</button>
-              </div>
-            )
-          })
-        }
-      </div>
+      <button onClick={goToAddTask} className='my-5 add-task-btn'>Add Task</button>
+      <table className='task-div'>
+        <thead>
+          <tr>
+            <td colSpan={2}>Tasks To Do</td>
+            <td>Status</td>
+            <td>Priority</td>
+            <td colSpan={2}>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            taskList.map((task, index) => {
+              return (
+                <tr key={index} className=''>
+                  <td>
+                    <input className='form-check-input' type='checkbox' checked={task.isChecked || false} onChange={() => handleCheckboxChange(index)} />
+                  </td>
+                  <td style={{ textDecoration: task.isChecked ? 'line-through' : 'none' }}>{task.name}</td>
+                  <td className={task.isChecked ? 'text-success' : 'text-danger'}>
+                    {task.isChecked ? "Complete" : "Incomplete"}
+                  </td>
+                  <td className={task.priority==='Low'?'bg-success':task.priority==='Medium'? 'bg-warning':'bg-danger'}>
+                    {task.priority}
+                  </td>
+                  <td>
+                    <button onClick={goToEditTask.bind(this, index)} disabled={task.isChecked} className='btn btn-info'>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick={deleteTask.bind(this, index)} disabled={task.isChecked} className='btn btn-danger'>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
