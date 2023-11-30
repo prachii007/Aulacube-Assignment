f6d5432edc09e6236ef1d291e6db6d1574df7d7c
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-30 11:29:04 +0530

    create and style seperate card component for each task

---
+++ src/components/singleTaskCard.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const SingleTaskCard = ({ handleCheckboxChange, goToEditTask, deleteTask, task, index }) => {
+    return (
+        <div className="card text-center mb-5">
+            <div className="card-header">
+                Task To Do
+            </div>
+            <div className="card-body">
+                <h5 className="card-title text-start">
+                    <input
+                        className='form-check-input me-2'
+                        type='checkbox'
+                        checked={task.isChecked || false}
+                        onChange={() => handleCheckboxChange(index)}
+                    />
+                    <span
+                        className='inline-block'
+                        style={{
+                            textDecoration:
+                                task.isChecked ? 'line-through' : 'none'
+                        }}>
+                        {task.name}
+                    </span>
+                </h5>
+                <p className='card-text text-start'
+                    style={{
+                        textDecoration: task.isChecked ? 'line-through' : 'none'
+                    }}>
+                    Description : {task.description}
+                </p>
+            </div>
+            <div className='card-text text-start ms-3'>
+                <p className={
+                    task.isChecked ?
+                        'text-success col-12 col-sm-3 mb-2 ps-2'
+                        : 'text-danger col-sm-3 col-12 mb-2 ps-2'}>
+                    Status : {task.isChecked ? "Complete" : "Incomplete"}
+                </p>
+                <p className={
+                    task.priority === 'Low' ?
+                        'bg-success col-10 col-sm-3 ps-2 priority'
+                        : task.priority === 'Medium' ?
+                            'bg-warning  col-10 col-sm-3 ps-2 priority'
+                            : 'bg-danger  col-10 col-sm-3 ps-2 priority'
+                }
+                >
+                    Priority : {task.priority}
+                </p>
+            </div>
+            <div className="text-body-secondary row justify-content-center mb-4">
+                <button
+                    onClick={goToEditTask.bind(this, index)}
+                    disabled={task.isChecked}
+                    className='edit action-btn mx-2  col-sm-2 col-8'
+                >
+                    Edit
+                </button>
+                <button
+                    onClick={deleteTask.bind(this, index)}
+                    disabled={task.isChecked}
+                    className='delete action-btn mx-2  col-sm-2 col-8'
+                >
+                    Delete
+                </button>
+            </div>
+        </div >
+    )
+}
+
+export default SingleTaskCard
--- src/components/taskList.js
+++ src/components/taskList.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import SingleTaskCard from './singleTaskCard'
 
 export const TaskList = () => {
 
@@ -15,7 +16,6 @@ export const TaskList = () => {
     updatedTasks[index].isChecked = !updatedTasks[index].isChecked
     localStorage.setItem('todos', JSON.stringify(updatedTasks));
     setTasklist(updatedTasks);
-    // setIsChecked(!isChecked)
   }
 
   const goToAddTask = () => {
@@ -37,42 +37,24 @@ export const TaskList = () => {
     <div className='text-center mt-5'>
       <h1>Task List</h1>
       <button onClick={goToAddTask} className='my-5 add-task-btn'>Add Task</button>
-      <table className='task-div'>
-        <thead>
-          <tr>
-            <td colSpan={2}>Tasks To Do</td>
-            <td>Status</td>
-            <td>Priority</td>
-            <td colSpan={2}>Actions</td>
-          </tr>
-        </thead>
-        <tbody>
+      <div className='outer-box'>
+        <div className=''>
           {
             taskList.map((task, index) => {
               return (
-                <tr key={index} className=''>
-                  <td>
-                    <input className='form-check-input' type='checkbox' checked={task.isChecked || false} onChange={() => handleCheckboxChange(index)} />
-                  </td>
-                  <td style={{ textDecoration: task.isChecked ? 'line-through' : 'none' }}>{task.name}</td>
-                  <td className={task.isChecked ? 'text-success' : 'text-danger'}>
-                    {task.isChecked ? "Complete" : "Incomplete"}
-                  </td>
-                  <td className={task.priority==='Low'?'bg-success':task.priority==='Medium'? 'bg-warning':'bg-danger'}>
-                    {task.priority}
-                  </td>
-                  <td>
-                    <button onClick={goToEditTask.bind(this, index)} disabled={task.isChecked} className='btn btn-info'>Edit</button>
-                  </td>
-                  <td>
-                    <button onClick={deleteTask.bind(this, index)} disabled={task.isChecked} className='btn btn-danger'>Delete</button>
-                  </td>
-                </tr>
+                <SingleTaskCard
+                  // className='single-row'
+                  key={index}
+                  index={index}
+                  handleCheckboxChange={handleCheckboxChange}
+                  goToEditTask={goToEditTask}
+                  deleteTask={deleteTask}
+                  task={task} />
               )
             })
           }
-        </tbody>
-      </table>
+        </div>
+      </div>
     </div>
   )
 }
